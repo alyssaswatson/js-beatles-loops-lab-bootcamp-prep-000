f153acb61b1df7d9b532af84194546b9f5642af0
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-11 23:03:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var phrases = [];
-  for (var i = 0; i < musicians.length; i++)
+  for (var i = 0; i < musicians.length; i++) {
   phrases.push (musicians[i] + "plays" + instruments[i]);
 
 }
