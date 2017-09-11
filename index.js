f2f7e1d7644fce2363f11c66d1ffac0e4b59d094
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-11 23:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function johnLennonFacts(facts) {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"
 ];
   var newfacts = [];
+  var i = 0
   while (i < facts.length) {
     newFacts.push (facts[i] + "!!!")
     i++;
