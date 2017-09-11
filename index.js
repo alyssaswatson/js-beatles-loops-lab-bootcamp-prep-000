9179588f7ce3e4e9cc305d416c6d49f79207e6e4
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-11 23:39:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"
-];
-  var newFacts = [];
-  var i = 0
+  var array = [];
+  let i = 0
   while (i < facts.length) {
-    newFacts.push (facts[i] + "!!!")
+    array.push (`${facts[i]}!!!`)
     i++;
   }
-  return newFacts;
+  return array
 }
